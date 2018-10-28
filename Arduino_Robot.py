93507590eed2a7f1cb63b8dc037465687e3a126b
Luke Bentley-Fox <lbentley-fox1@sheffield.ac.uk>
2018-10-28 13:40:06 +0000

    Add files via upload

--- Arduino_Robot.py
+++ Arduino_Robot.py
@@ -1,52 +1,51 @@
-
 import serial
-from time import Sleep
+from time import sleep
 
 ser = serial.Serial("com3", 9600, timeout=5)
 
 
 def Forward():
     sleep(1)
-    ser.write(b '1')
+    ser.write(b'1')
     return
 
 def PivotLeft(): #turn left
     sleep(1)
-    ser.write(b '2')
+    ser.write(b'2')
     return 
 
 def PivotRight(): #turn right
     sleep(1)
-    ser.write(b '3')
+    ser.write(b'3')
     return 
 
 def FLeft(): #full forward turning left
     sleep(1)
-    ser.write(b '4')
+    ser.write(b'4')
     return 
     
 def FRight(): #full forward turning right
     sleep(1)
-    ser.write(b '5')
+    ser.write(b'5')
     return 
     
 def BLeft(): #full backward turning left
     sleep(1)
-    ser.write(b '6')
+    ser.write(b'6')
     return 
     
 def BRight(): #full backward turning right
     sleep(1)
-    ser.write(b '7')
+    ser.write(b'7')
     return 
     
 def Stop(): #stop
     sleep(1)
-    ser.write(b '8')
+    ser.write(b'8')
     return 
 
 def Backward(): # backward straight
     sleep(1)
-    ser.write(b '9')
+    ser.write(b'9')
     return 
 
